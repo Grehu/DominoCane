83aea3e658f1bde842fc9312c77db9149aeeba66
Patryk <patryk.zalucki@gmail.com>
2017-05-06 21:17:03 +0200

    zoom + scroll works

--- app/src/main/java/hackatheon/beczka/dominocane/DominoCane.java
+++ app/src/main/java/hackatheon/beczka/dominocane/DominoCane.java
@@ -1,13 +1,31 @@
 package hackatheon.beczka.dominocane;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DominoCane extends AppCompatActivity {
+
+    public void reaction()
+    {
+        Intent i = new Intent(this, ScrollActivity.class);
+        startActivity(i);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_domino_cane);
+
+        Button b = (Button)findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reaction();
+            }
+        });
     }
 }
