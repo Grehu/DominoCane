cbf51febc6b0415ace91f535dac612c313b8f459
Patryk <patryk.zalucki@gmail.com>
2017-05-07 10:00:15 +0200

    Added main activity with images and layout TODO: make actions and instances in every functions

--- app/src/main/java/hackatheon/beczka/dominocane/DominoCane.java
+++ app/src/main/java/hackatheon/beczka/dominocane/DominoCane.java
@@ -1,17 +1,41 @@
 package hackatheon.beczka.dominocane;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.ImageButton;
 
-public class DominoCane extends AppCompatActivity {
+public class DominoCane extends Activity {
 
-    public void reaction()
+
+    //TODO: zaimplementowac ciala metod do poszczegolnych guzikow
+    public void camera()
     {
-        Intent i = new Intent(this, ScrollActivity.class);
-        startActivity(i);
+//        Intent i = new Intent(this, ScrollActivity.class);
+//        tartActivity(i);
+    }
+
+    public void exhibition()
+    {
+//        Intent i = new Intent(this, ScrollActivity.class);
+//        tartActivity(i);
+    }
+
+    public void map()
+    {
+//        Intent i = new Intent(this, ScrollActivity.class);
+//        tartActivity(i);
+    }
+
+    public void OP()
+    {
+//        Intent i = new Intent(this, ScrollActivity.class);
+//        tartActivity(i);
     }
 
 
@@ -20,20 +44,47 @@ public class DominoCane extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_domino_cane);
 
-        Button b = (Button)findViewById(R.id.button1);
-        b.setOnClickListener(new View.OnClickListener() {
+        ImageButton buttonCamera = (ImageButton)findViewById(R.id.imageButtonStart);
+        ImageButton buttonExhibition = (ImageButton)findViewById(R.id.imageButtonExpo);
+        ImageButton buttonMap = (ImageButton)findViewById(R.id.imageButtonMap);
+        ImageButton buttonOP = (ImageButton)findViewById(R.id.imageButtonOP);
+
+        buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                reaction();
-                Button informacje_szczegolowe = (Button) findViewById(R.id.button);
-
-                informacje_szczegolowe.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        startActivity(new Intent(DominoCane.this, Pop.class));
-                    }
-                });
+                camera();
             }
         });
+
+        buttonExhibition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                exhibition();
+            }
+        });
+
+        buttonMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                map();
+            }
+        });
+
+        buttonOP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                OP();
+            }
+        });
+
+//        Button informacje_szczegolowe = (Button) findViewById(R.id.button);
+//
+//        informacje_szczegolowe.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startActivity(new Intent(DominoCane.this, Pop.class));
+//            }
+//        });
+
     }
 }
